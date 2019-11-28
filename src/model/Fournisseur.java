ec7b63c34fda0c71793143a4239d5c6a9f4082ad
florian <gigotFlorian@gmail.com>
2019-11-28 11:15:02 +0100

    ..

--- src/model/Fournisseur.java
+++ src/model/Fournisseur.java
@@ -1,5 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Fournisseur extends Personne{
-	
+	private ArrayList<Produit> listeProduits ;
+
+	public Fournisseur() {
+		super();
+		this.listeProduits = listeProduits;
+	}
+
+	public Fournisseur(String nom, String prenom, String adresse, String ville) {
+		super(nom, prenom, adresse, ville);
+		this.listeProduits = listeProduits;
+	}
+
+	public Fournisseur(ArrayList<Produit> listeProduits) {
+		this.listeProduits = listeProduits;
+	}
+
+	public ArrayList<Produit> getListeProduits() {
+		return listeProduits;
+	}
+
+	public void setListeProduits(ArrayList<Produit> listeProduits) {
+		this.listeProduits = listeProduits;
+	}
 }
